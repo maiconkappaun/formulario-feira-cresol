82c2c99da3e53285afe2a5a6ab14a639017ceef8
maiconkappaun <98757617+maiconkappaun@users.noreply.github.com>
2023-09-21 08:47:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var axios = require('axios')
+
+function salvar(nome, cpf, telefone){
+     axios.post('https://sheetdb.io/api/v1/2wrscdvn2tr9y',{
+        "data": {
+            "Nome": nome, 
+            "CPF": cpf,
+            "Telefone": telefone
+        }
+     },{
+        "auth": {
+            "username": "437dt814",
+            "password": "ej2seew29jwzjstwb37h"
+        }
+     })
+}
